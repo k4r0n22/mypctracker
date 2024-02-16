3ed992fab29844749dc47db6058368c9ddebaa80
Carlos <cjgl2004@gmail.com>
2024-02-16 02:25:50 +0100

    db modificada

--- src/db/config.js
+++ src/db/config.js
@@ -1,5 +1,5 @@
 export const PORT = process.env.PORT || 3000
-export const DB_HOST = process.env.DB_HOST || 'monorail.proxy.rlwy.net'
+export const DB_HOST = process.env.DB_HOST || 'monorail.proxy.rlwy.net:29930'
 export const DB_USER = process.env.DB_USER || 'root'
 export const DB_PASSWORD = process.env.DB_PASSWORD || ''
 export const DB_NAME = process.env.DB_NAME || 'tracker'
